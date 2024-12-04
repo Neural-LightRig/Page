87becba2c03f25ca161e0d2c3a440af6a4e20f3b
Zexin He <jacques.zxhe@link.cuhk.edu.hk>
2024-12-04 20:14:07 +0800

    Dynamic set external link

---
+++ static/js/external-links.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('a.external-link');
+    links.forEach(link => {
+      link.setAttribute('target', '_top');
+    });
+});
